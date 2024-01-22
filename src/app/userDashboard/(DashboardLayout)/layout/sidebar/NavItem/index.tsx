6ea6419cba25fc214a6bd6c5a0969dcbaabb538b
Ankit Raj <ankitraja177@gmail.com>
2024-01-23 02:51:20 +0530

    Update NavItem styles to use primary palette

--- src/app/userDashboard/(DashboardLayout)/layout/sidebar/NavItem/index.tsx
+++ src/app/userDashboard/(DashboardLayout)/layout/sidebar/NavItem/index.tsx
@@ -46,14 +46,14 @@ const NavItem = ({ item, level, pathDirect, onClick }: ItemType) => {
       color: theme.palette.text.secondary,
       paddingLeft: "10px",
       "&:hover": {
-        backgroundColor: theme.palette.error.light,
-        color: theme.palette.error.main,
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.primary.main,
       },
       "&.Mui-selected": {
         color: "white",
-        backgroundColor: theme.palette.error.main,
+        backgroundColor: theme.palette.primary.main,
         "&:hover": {
-          backgroundColor: theme.palette.error.main,
+          backgroundColor: theme.palette.primary.main,
           color: "white",
         },
       },
